be58cbe4378c5d93e2342b5bc310e02cda975d13
MARQUEZ Jesu Amet HP <c.jmarquez@ternium.com>
2023-05-11 14:17:48 -0300

    selecciona una imagen desde el dispositivo

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -7,13 +9,31 @@ import ImageViewer from './components/ImageViewer';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage}/>
+    <View style={ styles.container }>
+      <View style={ styles.imageContainer }>
+        <ImageViewer 
+          placeholderImageSource={ PlaceholderImage }
+          selectedImage={ selectedImage }
+        />
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Chose a photo" theme="primary" />
+      <View style={ styles.footerContainer }>
+        <Button label="Chose a photo" theme="primary" onPress={ pickImageAsync } />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
