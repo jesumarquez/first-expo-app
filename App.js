8ca3dd0f28f3962422ef8a81b872f34d0c0eac38
MARQUEZ Jesu Amet HP <c.jmarquez@ternium.com>
2023-05-12 10:24:45 -0300

    PanGesture

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from './components/Button';
 import CircleButton from "./components/CircleButton";
@@ -53,7 +54,7 @@ export default function App() {
   }
 
   return (
-    <View style={ styles.container }>
+    <GestureHandlerRootView style={ styles.container }>
       <View style={ styles.imageContainer }>
         <ImageViewer 
           placeholderImageSource={ PlaceholderImage }
@@ -80,7 +81,7 @@ export default function App() {
         <EmojiList onSelect={ setPickedEmoji } onCloseModal={ onModalClose } />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
